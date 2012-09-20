052ff859b597cf5d784edbcb857aaf7ea6ff671c
holyredbeard <hp222bf@student.lnu.se>
2012-09-20 17:56:52 +0200

    Update TestAll.php
    Added tests to TestAll.php

--- lab2/login/test/TestAll.php
+++ lab2/login/test/TestAll.php
@@ -1,49 +1,56 @@
 <?php
 
-	// Funktion som innehåller ett antal enhetstester.
-	public function Test() {
-		
-		$this->DoLogout();	// loggar ut användaren som förberedelse för testerna.
-		
-		// Test 1: Kolla så att man inte är inloggad.
-		if($this->IsLoggedIn() == true){
-			echo "Test 1 misslyckades";
-			return false;
-		}
-		
-		// Test 2: Kolla så att det inte går att logga in med fel lösenord.
-		if ($this->DoLogin("henke", "4321") == true){
-			echo "Test 2 misslyckades";
-			return false;
-		}
-		
-		// Test 3: Kolla så att det går att logga in.
-		if ($this->DoLogin("henke", "1234") == false){
-			echo "Test 3 misslyckades";
-			return false;
-		}
+	require_once "./classes/LoginController.php";
 
-		// Test 4: Kolla så att man är inloggad.
-		if ($this->IsLoggedIn() == false){
-			echo "Test 4 misslyckades";
-			return false;
+	class TestAll {
+	
+		// Funktion som innehåller ett antal enhetstester.
+		public function Test() {
+
+			$LoginHandler = new LoginHandler();
+
+			$LoginHandler->DoLogout();	// loggar ut användaren som förberedelse för testerna.
+			
+			// Test 1: Kolla så att man inte är inloggad.
+			if($LoginHandler->IsLoggedIn()){
+				echo "Test 1: DoLogut(), misslyckades (är inloggad).";
+				return false;
+			}
+			
+			// Test 2: Kolla så att det inte går att logga in med fel lösenord.
+			if ($LoginHandler->DoLogin("henke", "4321")){
+				echo "Test 2: DoLogin(), misslyckades (det går att logga in med fel lösenord).";
+				return false;
+			}
+			
+			// Test 3: Kolla så att det går att logga in.
+			if ($LoginHandler->DoLogin("henke", "1234") == false){
+				echo "Test 3: DoLogin(), misslyckades (det går inte att logga in med korrekta uppgifter).";
+				return false;
+			}
+
+			// Test 4: Kolla så att man är inloggad.
+			if ($LoginHandler->IsLoggedIn() == false){
+				echo "Test 4: IsLoggedIn(), misslyckades (ej var inloggad).";
+				return false;
+			}
+
+			$LoginHandler->DoLogout();	// loggar ut användaren igen
+
+			// Test 5: Kolla så att man inte är inloggad.
+			if ($LoginHandler->IsLoggedIn()){
+				echo "Test 5: IsLoggedIn(), misslyckades (är fortfarande inloggad).";
+				return false;
+			}
+
+			// Test 6: Kolla så att man inte kan logga in med fel lösenord.
+			if ($LoginHandler->DoLogin("heke", "1235")){
+				echo "Test 6: DoLogin(), misslyckades (det gick att logga in med fel användarnamn och lösenord).";
+				return false;
+			}
+
+			return true;
 		}
-
-		$this->DoLogout();	// loggar ut användaren igen
-
-		// Test 5: Kolla så att man inte är inloggad.
-		if ($this->IsLoggedIn() == true){
-			echo "Test 5 misslyckades";
-			return false;
-		}
-
-		// Test 6: Kolla så att man inte kan logga in med fel lösenord.
-		if ($this->DoLogin("henke", "1235") == true){
-			echo "Test 6 misslyckades";
-			return false;
-		}
-
-		return true;
 	}
 
 ?>
